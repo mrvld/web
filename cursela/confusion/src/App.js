f6b572625b3261fa7ab9d6766c03627c6a4596c0
Vlad <mrvld1999@gmail.com>
2020-05-17 19:52:54 +0300

    add

---
+++ cursela/confusion/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+import './App.css';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
+
+class App extends Component {
+
+  render() {
+    return (
+    	<Provider store={store}>
+	        <BrowserRouter>
+	          <div className="App">
+	            <Main />
+	          </div>
+	        </BrowserRouter>
+      	</Provider>
+    );
+  }
+}
+
+export default App;
